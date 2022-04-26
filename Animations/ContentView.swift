32a61d054a0c5e0c2143a99214f2d081d532033a
Rodrigo Kroef tarouco <55713305+rodrigoktarouco@users.noreply.github.com>
2022-04-26 16:19:58 -0300

    Animations Intro

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -8,9 +8,61 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var animationAmount = 0.0
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        //        Button("Tap me") {
+        ////            animationAmount += 1
+        //        }
+        //        .padding(50)
+        //        .background(.red)
+        //        .foregroundColor(.white)
+        //        .clipShape(Circle())
+        //        .overlay(
+        //            Circle()
+        //                .stroke(.red)
+        //                .scaleEffect(animationAmount)
+        //                .opacity(2 - animationAmount)
+        //                .animation(
+        //                    .easeOut(duration: 1)
+        //                    .repeatForever(autoreverses: false), value: animationAmount
+        //                )
+        //        )
+        //        .onAppear {
+        //            animationAmount = 2
+        //        }
+        //        print(animationAmount)
+        //
+        //        return VStack {
+        //            Stepper("Scale amount", value: $animationAmount.animation(
+        //                .easeInOut(duration: 1)
+        //                .repeatCount(3, autoreverses: true)
+        //                ), in: 1...10)
+        //
+        //            Spacer()
+        //
+        //            Button("Tap Me") {
+        //                animationAmount += 1
+        //            }
+        //            .padding(40)
+        //            .background(.red)
+        //            .foregroundColor(.white)
+        //            .clipShape(Circle())
+        //            .scaleEffect(animationAmount)
+        //        }
+        
+        Button("Tap Me") {
+            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+                animationAmount += 360
+            }
+        }
+        .padding(50)
+        .background(.red)
+        .foregroundColor(.white)
+        .clipShape(Circle())
+        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 0, z: 1))
+        
     }
 }
 
