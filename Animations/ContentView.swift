d9bb15ce0b154b7ed2df8ec2fe4aa48031be75bd
Rodrigo Kroef tarouco <55713305+rodrigoktarouco@users.noreply.github.com>
2022-04-28 17:28:04 -0300

    Animations

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var animationAmount = 0.0
+    @State private var enabled = false
     
     var body: some View {
         
@@ -51,17 +52,60 @@ struct ContentView: View {
         //            .clipShape(Circle())
         //            .scaleEffect(animationAmount)
         //        }
-        
-        Button("Tap Me") {
-            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
-                animationAmount += 360
+//        Button("Tap Me") {
+//            withAnimation(.interpolatingSpring(stiffness: 5, damping: 1)) {
+//                animationAmount += 360
+//            }
+//        }
+//        .padding(50)
+//        .background(.red)
+//        .foregroundColor(.white)
+//        .clipShape(Circle())
+//        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 0, z: 1))
+        VStack {
+            Button("Tap Me") {
+                enabled.toggle()
             }
+            .background(enabled ? .blue : .red)
+            .frame(width: 200, height: 200)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+            .animation(.default, value: enabled)
+            
+            Button("Tap Me") {
+                enabled.toggle()
+            }
+            .frame(width: 200, height: 200)
+            .background(enabled ? .blue : .red)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+            .animation(.default, value: enabled)
+            
+            Button("Tap Me") {
+                enabled.toggle()
+            }
+            .frame(width: 200, height: 200)
+            .background(enabled ? .blue : .red)
+            .animation(.default, value: enabled)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+            .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
+            
+            Button("Tap Me") {
+                enabled.toggle()
+            }
+            .frame(width: 200, height: 200)
+            .background(enabled ? .blue : .red)
+            .animation(nil, value: enabled)
+            .foregroundColor(.white)
+            .clipShape(RoundedRectangle(cornerRadius: enabled ? 60 : 0))
+            .animation(.interpolatingSpring(stiffness: 10, damping: 1), value: enabled)
+            
+
+            
         }
-        .padding(50)
-        .background(.red)
-        .foregroundColor(.white)
-        .clipShape(Circle())
-        .rotation3DEffect(.degrees(animationAmount), axis: (x: 0, y: 0, z: 1))
+        
+        
         
     }
 }
